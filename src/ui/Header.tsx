7ff0a32c474f690bc2e8e48207eb6a6b4aafc5f8
Alejandro Castillo <alejandrocastillo252006@gmail.com>
2025-02-21 20:12:24 -0400

    Adding Jason viewer v0.1

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -12,16 +12,19 @@ import { SidebarContent } from "./SidebarContent";
 
 function OpenOptionsBtn() {
   return (
-    <Sheet >
-      <SheetTrigger className="ml-auto">
-        <Button
+    <Sheet>
+      <SheetTrigger
+        className="ml-auto aspect-square h-[100%]"
+        id="open-sidebar-btn"
+      >
+        {/* <Button
           variant={"ghost"}
           size={"icon"}
           id="open-sidebar-btn"
           className="aspect-square h-[3.5rem] w-[3.5rem]"
-        >
-          <GetIcon name="Burger" />
-        </Button>
+        > */}
+        <GetIcon name="Burger" />
+        {/* </Button> */}
       </SheetTrigger>
       <SidebarContent />
     </Sheet>
@@ -31,7 +34,7 @@ function OpenOptionsBtn() {
 export function Header() {
   return (
     <>
-      <header className="items-center flex flex-row h-[4rem] pl-5 bg-(--primary) border-b-4 borderb-(--border)">
+      <header className="sticky top-0 z-10 w-vw items-center flex flex-row h-[4rem] min-h-[4rem] pl-5 bg-(--primary) border-b-4 borderb-(--border)">
         <h1 className=" font-semibold text-4xl">Jason</h1>
         <OpenOptionsBtn />
       </header>
--- src/ui/Main.tsx
+++ src/ui/Main.tsx
@@ -1,9 +1,80 @@
+import { AppContext } from "@/AppContext";
+import { useContext } from "react";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+// import { Separator } from "@/components/ui/separator";
+
+
+//TODO Add edit ... Button to each element
+function getRandom() {
+  return crypto.randomUUID();
+}
+
+
+
+
+
+
+function handleJason(jason: Record<string, unknown>) {
+  const res: Array<JSX.Element> = [];
+  
+  
+
+  for (const [k, v] of Object.entries(jason)) {
+    if (v === null || typeof v !== "object") {
+      res.push(
+        // <Accordion type="single" collapsible  className="w-max">
+        //   <AccordionItem value={`item-${getRandom()}`}>
+        //     <AccordionTrigger>{k}</AccordionTrigger>
+        //     <AccordionContent className="ml-1 w-max">
+        //       {v as any}
+        //     </AccordionContent>
+        //   </AccordionItem>
+        // </Accordion>
+        <>
+          {/* <Separator orientation="horizontal" className="mt-1" /> */}
+
+          <div className="group bg-(--secondary) p-2 rounded-(--radius) w-max text-[1rem]">
+            <span className="font-semibold group-hover:underline">{k}</span>
+            {" :  "}
+            <span>{v === null ? "null" : (v === undefined ? "undefined" : v.toString())}</span>
+          </div>
+        </>
+      );
+    } else {
+      const rand = getRandom();
+      res.push(
+        <>
+          <Accordion type="single" collapsible className="w-max">
+            <AccordionItem value={`item-${rand}`} itemID={rand}>
+              <AccordionTrigger className="bg-(--secondary) p-2 m-0 text-[1rem]">
+                {k}{" :"}
+              </AccordionTrigger>
+              <AccordionContent className=" ml-2 w-max p-0">
+                {handleJason(v as typeof jason)}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </>
+      );
+
+    }
+  }
+
+  return res;
+}
 
 export function Main() {
+  const [jason, setJason] = useContext(AppContext)?.jason!;
+
   return (
     <>
-      <main>MAIN!</main>
+      <main className="flex-grow p-2">{handleJason(jason as Record<string, unknown>)}</main>
     </>
-  )
+  );
 }
